f3d55fdeaa5c7ef252799a399fa348375e9fb7cc
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-05-17 14:54:14 +0530

    add function for song update

--- script.js
+++ script.js
@@ -153,135 +153,58 @@ console.log(audio.querySelector("audio").querySelector("source").src);
 
 
 const song2 = document.querySelector(".song2");
-song2.addEventListener("click", function () {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song2.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+song2.addEventListener("click", () => {
+  updateSong(song2);
 });
 
 const song1  = document.querySelector(".song1");
-song1.addEventListener("click", function () {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song1.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+song1.addEventListener("click", () => {
+  updateSong(song1);
 });
 
 const song3 = document.querySelector(".song3");
-song3.addEventListener("click", function () {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song3.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+song3.addEventListener("click", () => {
+  updateSong(song3);
 });
 
 const song4 = document.querySelector(".song4");
-song4.addEventListener("click", function () {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song4.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+song4.addEventListener("click", () => {
+  updateSong(song4);
 });
 
 const song5 = document.querySelector(".song5");
-song5.addEventListener("click", function () {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song5.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+song5.addEventListener("click", () => {
+  updateSong(song5);
 });
 
 const song6 = document.querySelector(".song6");
-song6.addEventListener("click", function () {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song6.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+song6.addEventListener("click", () => {
+  updateSong(song6);
 });
 
 const song7 = document.querySelector(".song7");
-song7.addEventListener("click", function () {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song7.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+song7.addEventListener("click", () => {
+  updateSong(song7);
 });
 
 const song8 = document.querySelector(".song8");
-song8.addEventListener("click", function () {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song8.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+song8.addEventListener("click", () => {
+  updateSong(song8);
 });
 
 const song9 = document.querySelector(".song9");
-song9.addEventListener("click", function () {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song9.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+song9.addEventListener("click", () => {
+  updateSong(song9);
 });
 
 const song10 = document.querySelector(".song10");
-song10.addEventListener("click", function () {
+song10.addEventListener("click", () => {
+  updateSong(song10);
+});
+
+function updateSong(song) {
   songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song10.textContent.toLowerCase()) {
+    if (temp.name.toLowerCase() === song.textContent.toLowerCase()) {
       audio.querySelector("img").src = temp.image;
       audio.querySelector("h1").textContent = temp.name;
       audio.querySelector("p").textContent = temp.artist;
@@ -290,7 +213,7 @@ song10.addEventListener("click", function () {
       audio.querySelector("audio").play();
     }
   });
-});
+}
 
  
 
