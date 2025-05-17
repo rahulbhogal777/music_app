53df8130e6f757a98e3e3e533cc7b3f620a521ce
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-05-17 20:47:04 +0530

    add new updated

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ const selectGenre = document.querySelector("#genre");
 const songs = document.querySelector(".songs");
 const audio = document.querySelector(".audio-player");
 
-
+// 
 function renderList(filterList) {
   songs.innerHTML = "";
   filterList.forEach((temp) => {
@@ -128,72 +128,27 @@ selectGenre.addEventListener("change", function () {
   }
 });
 
-
-
-
-
-const song2 = document.querySelector(".song2");
-song2.addEventListener("click", () => {
-  updateSong(song2);
-});
-
-const song1  = document.querySelector(".song1");
-song1.addEventListener("click", () => {
-  updateSong(song1);
-});
-
-const song3 = document.querySelector(".song3");
-song3.addEventListener("click,", () => {
-  updateSong(song3);
-});
-
-const song4 = document.querySelector(".song4");
-song4.addEventListener("click", () => {
-  updateSong(song4);
-});
-
-const song5 = document.querySelector(".song5");
-song5.addEventListener("click", () => {
-  updateSong(song5);
-});
-
-const song6 = document.querySelector(".song6");
-song6.addEventListener("click", () => {
-  updateSong(song6);
-});
-
-const song7 = document.querySelector(".song7");
-song7.addEventListener("click", () => {
-  updateSong(song7);
-});
-
-const song8 = document.querySelector(".song8");
-song8.addEventListener("click", () => {
-  updateSong(song8);
-});
-
-const song9 = document.querySelector(".song9");
-song9.addEventListener("click", () => {
-  updateSong(song9);
-});
-
-const song10 = document.querySelector(".song10");
-song10.addEventListener("click", () => {
-  updateSong(song10);
-});
-
+// Function to update the song details in the audio player
 function updateSong(song) {
-  songList.forEach((temp) => {
-    if (temp.name.toLowerCase() === song.textContent.toLowerCase()) {
-      audio.querySelector("img").src = temp.image;
-      audio.querySelector("h1").textContent = temp.name;
-      audio.querySelector("p").textContent = temp.artist;
-      audio.querySelector("audio").querySelector("source").src = temp.audio;
-      audio.querySelector("audio").load();
-      audio.querySelector("audio").play();
-    }
-  });
+  const songName = song.textContent.toLowerCase();
+  const selectedSong = songList.find((temp) =>
+    temp.name.toLowerCase() === songName
+  );
+  audio.querySelector("img").src = selectedSong.image;
+  audio.querySelector("h1").textContent = selectedSong.name;
+  audio.querySelector("p").textContent = selectedSong.artist;
+  audio.querySelector("audio").querySelector("source").src = selectedSong.audio;
+  audio.querySelector("audio").load();
+  audio.querySelector("audio").play();
 }
+
+// Add event listener to the songs list
+songs.addEventListener("click", function (event) {
+  console.log(event.target.tagName.toLowerCase());
+  if (event.target.tagName.toLowerCase() === "p") {
+    updateSong(event.target);
+  }
+});
 
  
 
